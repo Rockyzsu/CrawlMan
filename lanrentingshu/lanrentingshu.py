4c18a5552f6447d17a44f87872b6e16afaa0b1b2
chenjw <chenjw@jpush.cn>
2017-09-13 21:07:25 +0800

    find the download link

--- lanrentingshu/lanrentingshu.py
+++ lanrentingshu/lanrentingshu.py
@@ -1,15 +1,25 @@
 # coding: utf-8
+import urllib
+
 import requests
+from lxml import etree
 from toolkit import getheader
 def spider():
-    for i in range(4):
+    for i in range(1,100,10):
         url='http://www.lrts.me/ajax/playlist/2/32551/%d' %i
         headers={'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36'}
         s=requests.get(url=url,headers=headers)
-        #js= s.json()
-        #data=js['data']
-        print s.text
+        tree=etree.HTML(s.text)
+        nodes=tree.xpath('//div[starts-with(@class,"clearfix section-item section_"]')
+        print len(nodes)
+        for node in nodes:
+            link=node.xpath('.//input[@name="source" and @type="hidden"]/@value')[0]
+            filename=node.xpath('.//span/text()')[0].encode('utf-8')
+            print link
+            print filename
+            #urllib.urlretrieve(link, filename=)
 
-spider()
+if __name__=='__main__':
+    spider()
 
 
