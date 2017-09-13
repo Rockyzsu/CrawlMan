65915f4aeb39987c2875b831c5f995c8e98fea68
Rockyzsu <jinweizsu@gmail.com>
2017-09-14 01:08:00 +0800

    first version

--- lanrentingshu/lanrentingshu.py
+++ lanrentingshu/lanrentingshu.py
@@ -1,26 +1,35 @@
 # coding: utf-8
 import urllib
 
+import os
 import requests
+import time
 from lxml import etree
 from header_toolkit import getheader
 
 
 def spider():
+    curr=os.getcwd()
+    target_dir=os.path.join(curr,'data')
+    if not os.path.exists(target_dir):
+        os.mkdir(target_dir)
     for i in range(1, 100, 10):
         url = 'http://www.lrts.me/ajax/playlist/2/32551/%d' % i
         headers = {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36'}
         s = requests.get(url=url, headers=headers)
         tree = etree.HTML(s.text)
-        nodes = tree.xpath('//div[starts-with(@class,"clearfix section-item section_"]')
+        nodes = tree.xpath('//*[starts-with(@class,"clearfix section-item section")]')
         print len(nodes)
         for node in nodes:
+            filename = node.xpath('.//div[@class="column1 nowrap"]/span/text()')[0]
             link = node.xpath('.//input[@name="source" and @type="hidden"]/@value')[0]
-            filename = node.xpath('.//span/text()')[0].encode('utf-8')
+
             print link
-            print filename
-            # urllib.urlretrieve(link, filename=)
+            post_fix=link.split('.')[-1]
+            full_path= filename+'.'+post_fix
+            urllib.urlretrieve(link, filename=os.path.join(target_dir,full_path))
+            time.sleep(1)
 
 
 if __name__ == '__main__':
