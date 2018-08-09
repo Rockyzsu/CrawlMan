ce828d4f93c0c7ba63c7cca9fe7c4f5d1e4c671e
Rocky Chen <jinweizsu@gmail.com>
2018-08-09 18:05:25 +0800

    update

---
+++ jd/jd/pipelines.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
+from jd.items import JdItem
+import pymongo
+class JDPipeline(object):
+    def __init__(self):
+        self.mongo=pymongo.MongoClient('10.18.6.102')
+        self.doc=self.mongo['spider']['jd_book']
+    def process_item(self, item, spider):
+        self.doc.insert(dict(item))
+
+        return item
