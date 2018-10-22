6ab4586890519d910f0a3255fe93199bea62b75b
Rockyzsu <jinweizsu@gmail.com>
2018-10-22 23:15:57 +0800

    update

--- chinaclear/chinaclear.py
+++ chinaclear/chinaclear.py
@@ -47,7 +47,10 @@ def llogger(filename):
 logger = llogger(__file__)
 
 
-def get_value():
+def get_value(year):
+    option_dict = {'2015':2,'2018':1}
+    option=option_dict.get(year,'2018')
+
     try:
         s = session.get(url=home_page, headers=headers)
     except Exception as e:
@@ -55,16 +58,18 @@ def get_value():
         return None
     # print(s.text)
     root = etree.HTML(s.text)
-    value = root.xpath('//select[@name="channelIdStr"]/option[1]/@value')[0]
+    value = root.xpath('//select[@name="channelIdStr"]/option[{}]/@value'.format(option))[0]
     # print(value)
     return value
 
 
-def main():
-    value = get_value()
+
+def year2018():
+    year='2018'
+    value = get_value(year)
     # 第一次循环获取所有的数据
     crawl_time = datetime.datetime.now().strftime('%Y-%m-%d')
-    current = datetime.datetime.strptime('2016-03-04', '%Y-%m-%d')
+    current = datetime.datetime.strptime('2015-05-29', '%Y-%m-%d')
     columns = ['新增投资者数量', '新增投资者数量-自然人', '新增投资者数量-非自然人',
                '期末投资者数量', '期末投资者数量-自然人', '已开立A股账户投资者-自然人', '已开立B股账户投资者-自然人',
                '期末投资者数量-非自然人', '已开立A股账户投资者-非自然人', '已开立B股账户投资者-非自然人',
@@ -77,7 +82,7 @@ def main():
     for i in range(0, 365*4, 7):
         now = current + datetime.timedelta(days=-1 * i)
         now_str = now.strftime('%Y.%m.%d')
-        if now_str=='2015.05.29':
+        if now_str=='2014.05.29':
             break
         print(now_str)
         data = {
@@ -110,7 +115,7 @@ def main():
             # logger.error(content)
             # print(content)
         d = {}
-        d['publish_date']=now_str
+        d['publish_date']=now
         for idx, name in enumerate(columns):
             try:
                 print(name, '\t', num_list[idx])
@@ -137,4 +142,96 @@ def main():
             logger.error('异常 >>>{}'.format(e))
 
 
-main()
+def year2015():
+    year='2015'
+    value = get_value(year)
+    # 第一次循环获取所有的数据
+    crawl_time = datetime.datetime.now().strftime('%Y-%m-%d')
+    current = datetime.datetime.strptime('2009-06-12', '%Y-%m-%d')
+    columns = ['期末有效账户数（万户）',
+               '新增股票账户数（户）合计',
+               '新增A股开户数（户）',
+               '新增B股开户数（户）',
+                '期末股票账户数（万户）',
+               '1、期末A股账户数（万户）',
+               '（1）期末持仓A股账户数（万户）',
+               '（2）本周参与交易的A股账户数（万户）',
+               '2、期末B股账户数（万户）',
+               '期末休眠账户数（万户）',
+               ]
+    doc = db['db_stock']['investor_trend']
+    # 2017.02.10 后面的只有10项
+
+    for i in range(0, 365*2, 7):
+        now = current + datetime.timedelta(days=-1 * i)
+        now_str = now.strftime('%Y.%m.%d')
+        if now_str=='2014.05.29':
+            break
+        print(now_str)
+        data = {
+            'dateType': '',
+            'dateStr': now_str,
+            'channelIdStr': value,
+        }
+        try:
+            s = session.post(url=home_page, headers=headers, data=data)
+        except Exception as e:
+            logger.error(e)
+            continue
+
+        if '没有找到相关信息' in s.text:
+            logger.info('没有找到相关信息')
+            continue
+
+        # print(s.text)
+        root = etree.HTML(s.text)
+        content = root.xpath('string(.)')
+        # content=re.search('期末有效账户数（万户）(.*)',content,re.S).group(1)
+        # print(content)
+        # 共有10个数字，分 别对应网站上的
+        num_list = re.findall('<SPAN.*?>([\d\.\,]+)\</SPAN>',s.text,re.S|re.I)
+        # num_list = re.findall('([\d*\.*,*\-*]+)\s+', content,re.S)
+        # num_list = re.findall('>([\d+\.,]+\S?)<', s.text)
+        # print(num_list)
+        if not num_list:
+            continue
+        num_list_copy = list(num_list)
+        for i in num_list:
+            if len(i)==1 and re.search('\d',i):
+                num_list_copy.remove(i)
+
+        l = len(num_list_copy)
+        if l!=30:
+            logger.warning('length not equal 30')
+            print(num_list_copy)
+            # continue
+            # logger.error(content)
+            # print(content)
+        d = {}
+        d['publish_date']=now
+        for idx in range(10):
+            try:
+                print(columns[idx], '\t', num_list_copy[2+3*idx])
+            except Exception as e:
+                logger.error(e)
+                continue
+            try:
+                 item = re.sub(',','',num_list_copy[idx])
+                 # if len(item.split('.')[1])>2:
+                 #     item = item[:len(item)-1]
+            except Exception as e:
+                logger.error(e)
+                d[columns[idx]]=0
+
+            else:
+                d[columns[idx]]=item
+
+        d['crawl_time'] = crawl_time
+
+
+        try:
+            doc.insert(d)
+        except Exception as e:
+            logger.error('异常 >>>{}'.format(e))
+
+year2015()
