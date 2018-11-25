7fc453c9ef213272b70fc9700d51fb0357b2f1da
Rocky Chen <jinweizsu@gmail.com>
2018-11-25 19:19:16 +0800

    update

--- MyLibrary/login.py
+++ MyLibrary/login.py
@@ -1,11 +1,10 @@
 import requests
 import hashlib
+import config
 
-# import config
-
-username = 'F44010006{}'
-# password=config.password
-s = bytes('', encoding='utf8')
+username = ''
+password=''
+s = bytes(password, encoding='utf8')
 m = hashlib.md5()
 m.update(s)
 first_md5 = m.hexdigest()
@@ -13,31 +12,22 @@ headers = {'Referer': 'https://www.szlib.org.cn/MyLibrary/Reader-Access.jsp?info
            'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36',
            'X-Requested-With': 'XMLHttpRequest'}
 
-import redis
-#
-# r = redis.StrictRedis('10.18.6.26', db=5, decode_responses=True)
-#
-# # for i in range(10000):
-# # 	username_crash=username.format(str(i).zfill(4))
-# # 	r.lpush('username',username_crash)
-# url = 'https://www.szlib.org.cn/MyLibrary/readerLoginM.jsp'
-# data = {'rand': '',
-#         'username': username,
-#         'password': first_md5,
-#
-#         }
-# print(data)
-# r = requests.post(url=url, headers=headers, data=data, timeout=15)
-# print(r.text)
-# if '<message>OK</message>' in r.text:
-#     print('Crash !!!')
-#     print(username)
-#     print(password)
-# # break
+url = 'https://www.szlib.org.cn/MyLibrary/readerLoginM.jsp'
+data = {'rand': '',
+        'username': username,
+        'password': first_md5,
 
+        }
+print(data)
 
-r = redis.StrictRedis('10.18.6.26', db=5, decode_responses=True)
+def login_session():
+    session = requests.Session()
 
-for i in range(10000):
-	username_crash=username.format(str(i).zfill(4))
-	r.lpush('username',username_crash)
+    r = session.post(url=url, headers=headers, data=data, timeout=15)
+    print(r.text)
+    if '<message>OK</message>' in r.text:
+        print('Crash !!!')
+        print(username)
+        print(password)
+
+    return session
--- MyLibrary/main.py
+++ MyLibrary/main.py
@@ -1,41 +1,58 @@
 # -*-coding=utf-8-*-
 import json
 import re
+import time
+
 import requests
 from lxml import etree
 import pandas as pd
 from collections import OrderedDict
 from sqlalchemy import create_engine
+from login import login_session
 
-engine=create_engine('mysql+pymysql://{}:{}@localhost:3306/db_rocky?charset=utf8'.format('root',''))
+engine = create_engine('mysql+pymysql://{}:{}@localhost:3306/db_rocky?charset=utf8'.format('root', '123456z'))
+session = login_session()
+
+
 def getHistory(start, end):
-    with open('cookies', 'r') as f:
-        # js = json.load(f)
-        js=eval(f.read())
-    # cookie=js.get('Cookie','')
-    headers = js.get('headers', '')
+    global session
+    # with open('cookies', 'r') as f:
+    #     # js = json.load(f)
+    #     js=eval(f.read())
+    # # cookie=js.get('Cookie','')
+    # headers = js.get('headers', '')
     # url='https://www.szlib.org.cn/MyLibrary/LoanHistory.jsp?v_StartDate=20090123&v_EndDate=20180123&v_ServiceAddr=&CardOrBarcode=cardno&cardno=0440070074317&v_LoanType=E&curpage=2'
+    headers = {
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+        'Accept-Encoding': 'gzip,deflate,br', 'Accept-Language': 'zh,en;q=0.9,en-US;q=0.8',
+        'Cache-Control': 'no-cache', 'Connection': 'keep-alive',
+        'Host': 'www.szlib.org.cn', 'Pragma': 'no-cache',
+        'Referer': 'https://www.szlib.org.cn/MyLibrary/Loan-Status.jsp', 'Upgrade-Insecure-Requests': '1',
+        'User-Agent': 'Mozilla/5.0(WindowsNT6.1;WOW64)AppleWebKit/537.36(KHTML,likeGecko)Chrome/67.0.3396.99Safari/537.36',
+        'X-Requested-With': 'XMLHttpRequest'
+    }
+    time.sleep(5)
     base = "https://www.szlib.org.cn/MyLibrary/LoanHistory.jsp?v_StartDate={}&v_EndDate={}&v_ServiceAddr=&CardOrBarcode=cardno&cardno=0440070074317&v_LoanType=Ea&curpage={}"
     url = base.format(start, end, 1)
-    r = requests.get(url=url, headers=headers)
+    r = session.get(url=url, headers=headers)
 
     # df = {'date': date, 'time': borrow_time, 'Book': title, 'optime': optype, 'From Library': cirtype, 'Address': addr,
     #       'Barcode': barcode, 'callno': callno}
     # col_name={'date':}
 
+    total = re.findall(r'<totalno>(\d+)</totalno>', r.text)[0]
+    date = []
+    borrow_time = []
+    optype = []
+    cirtype = []
+    title = []
+    addr = []
+    barcode = []
+    callno = []
 
-    total=re.findall(r'<totalno>(\d+)</totalno>',r.text)[0]
-    date=[]
-    borrow_time=[]
-    optype=[]
-    cirtype=[]
-    title=[]
-    addr=[]
-    barcode=[]
-    callno=[]
-
-    for page in range(1,(int(total)+20)//20+1):
-        r=requests.get(url=base.format(start,end,page),headers=headers)
+    for page in range(1, (int(total) + 20) // 20 + 1):
+        time.sleep(5)
+        r = session.get(url=base.format(start, end, page), headers=headers)
         tree = etree.HTML(r.text)
         for item in tree.xpath('//record'):
             if item.xpath('.//title/text()'):
@@ -49,14 +66,17 @@ def getHistory(start, end):
                 barcode.append(item.xpath('.//barcode/text()')[0])
                 callno.append(item.xpath('.//callno/text()')[0])
 
-
-    od=OrderedDict([('Date',date),('Time',borrow_time),('Book',title),('Optime',optype),('From Library',cirtype),('Address',addr),('Barcode',barcode),('Callno',callno)])
+    od = OrderedDict(
+        [('Date', date), ('Time', borrow_time), ('Book', title), ('Optime', optype), ('From Library', cirtype),
+         ('Address', addr), ('Barcode', barcode), ('Callno', callno)])
     print(od)
-    df=pd.DataFrame(od)
-    df.to_sql('library',engine)
+    df = pd.DataFrame(od)
+    print(df)
+    df.to_sql('tb_library', engine)
+
 
 def main():
-    getHistory('20100101', '20180822')
+    getHistory('20100101', '20181030')
 
 
 if __name__ == '__main__':
