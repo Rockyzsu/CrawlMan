6f0e62d6c6e4e01e380fe3c393b61c6851e4b645
Rocky Chen <jinweizsu@gmail.com>
2019-03-18 09:27:41 +0800

    股权数据

--- stock_pledge/crawler.py
+++ stock_pledge/crawler.py
@@ -2,38 +2,78 @@
 # website: http://30daydo.com
 # @Time : 2019/3/9 17:17
 # @File : crawler.py
+import datetime
+import requests
 
-import grequests
+# import grequests
 import pandas as pd
 import numpy as np
 from setting import get_engine
-url = 'http://www.chinaclear.cn/cms-rank/downloadFile?queryDate=2019.03.09&type=proportion'
+import tushare as ts
+
+# 2018.03.05 后才有数据
+
+url = 'http://www.chinaclear.cn/cms-rank/downloadFile?queryDate={}&type=proportion'
 
 headers = {'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
            'Accept-Encoding': 'gzip,deflate', 'Accept-Language': 'zh-CN,zh;q=0.9', 'Cache-Control': 'no-cache',
            'Pragma': 'no-cache', 'Proxy-Connection': 'keep-alive',
-           'Referer': 'http://www.chinaclear.cn/cms-rank/queryPledgeProportion?action=query&queryDate=2019.03.09&secCde=&page=3',
+           # 'Referer': 'http://www.chinaclear.cn/cms-rank/queryPledgeProportion?action=query&queryDate=2019.03.09&secCde=&page=3',
            'Upgrade-Insecure-Requests': '1',
-           'User-Agent': 'Mozilla/5.0(WindowsNT6.1;Win64;x64)AppleWebKit/537.36(KHTML,likeGecko)Chrome/68.0.3440.106Safari/537.36'}
-engine = get_engine('db_pledge','local')
+           'User-Agent': 'Mozilla/5.0(Win64;x64)AppleWebKit/537.36(KHTML,likeGecko)Chrome/68.0.3440.106Safari/537.36'}
+
+engine = get_engine('db_pledge', 'local')
+
 
 class PledgeSpider():
 
+    def __init__(self):
+        self.start = datetime.datetime.now()
+        self.delta= 400
+
+
+    def start_task(self):
+        pass
+
+    def handle_exception(self,request,exception):
+        print('process error')
+
     def crawl(self):
-        tasks=[grequests.get(url=url,headers=headers)]
-        resp = grequests.map(tasks)
-        for item in resp:
-            with open('pledge.xls','wb') as f:
-                f.write(item.content)
+        # tasks=[]
+        # date_list =[]
+        for i in range(self.delta):
+            fetch_day = self.start+datetime.timedelta(days=-1*i)
+            if fetch_day < datetime.datetime(year=2018,month=3,day=4):
+                break
+
+            if not ts.is_holiday(fetch_day.strftime('%Y-%m-%d')):
+                name=fetch_day.strftime('%Y-%m-%d')
+                try:
+                    day=url.format(fetch_day.strftime('%Y.%m.%d'))
+                    print(day)
+                    r=requests.get(url=day,headers=headers,timeout=20)
+                except Exception as e:
+                    print(e)
+                else:
+                    print(r.status_code)
+                    with open('{}.xls'.format(name), 'wb') as f:
+                        f.write(r.content)
+                # tasks.append(grequests.get(url=url.format(fetch_day.strftime('%Y.%m.%d'))))
+
+            # date_list.append(fetch_day.strftime('%Y-%m-%d'))
+
+        # resp = grequests.map(tasks,size=8,exception_handler=self.handle_exception)
+        # for index,r in enumerate(resp):
+        #     with open('{}.xls'.format(date_list[index]),'wb') as f:
+        #         f.write(r.content)
 
 
     def data_transfer(self):
         df = pd.read_excel('pledge.xls', header=2, dtype={'证券代码': np.str})
-        # print(df.head())
         df = df.reset_index(drop=True)
-        df.to_sql('')
         return df
+
+
 pledge = PledgeSpider()
-df = pledge.data_transfer()
-print(df)
-print(df.info())
+pledge.crawl()
+# df = pledge.data_transfer()
