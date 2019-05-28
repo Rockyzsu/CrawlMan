3e15bd6e7ac5c42351f39b1614303f3be0603f25
Rocky Chen <jinweizsu@gmail.com>
2019-05-28 19:05:01 +0800

    update

--- htqyy/main.py
+++ htqyy/main.py
@@ -1,102 +1,140 @@
-import requests,re,os
+import requests, re, os
 from lxml import etree
-headers={'User-Agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/37.0.2062.120 Chrome/37.0.2062.120 Safari/537.36'}
+import sys
+headers = {
+    "Accept": "text/plain, */*; q=0.01",
+    "Accept-Encoding": "gzip, deflate",
+    "Accept-Language": "zh,en;q=0.9,en-US;q=0.8,zh-CN;q=0.7",
+    "Cache-Control": "no-cache",
+    # "Cookie": "Hm_lvt_74e11efe27096f6ef1745cd53f168168=1558946189; blk=1; __cfduid=d996f76d6c8f4b2ddaf74e4c8d0980f0e1558946190; Hm_lpvt_74e11efe27096f6ef1745cd53f168168=1558948519",
+    "Host": "www.htqyy.com",
+    "Pragma": "no-cache",
+    # "Proxy-Connection": "keep-alive",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36",
+    "X-Requested-With": "XMLHttpRequest",
+}
 
-def saveToFile(r,filename):
-	if os.path.exists(filename):
-		return False
-	if not r:
-		return False
-	with open(filename,'wb') as f:
-		f.write(r.content)
-		return True
 
-def download(url,retry=3):
-	
-	try:
-		r=requests.get(url=url,headers=headers,timeout=60*10)
-		if r.status_code ==200:
-			return r
-		else:
-			print "Not able to download, status_code is :",r.status_code
-			if retry >0 :
-				download(url,retry-1)
-			else:
-				return None
-	except Exception,e:
-		print 'exception happend!'
-		print e
-		if retry >0:
-			download(url,retry-1)
+def saveToFile(r, filename):
+    if os.path.exists(filename):
+        return False
+    if not r:
+        return False
+    filename=re.sub('[\/:*?"<>|]', '-', filename)
+    with open(filename, 'wb') as f:
+        f.write(r.content)
+        return True
 
-		else:
-			return None
+
+
+def download(url, retry=3):
+    try:
+        r = requests.get(url=url, headers=headers, timeout=60 * 10)
+        if r.status_code == 200:
+            return r
+        else:
+            print("Not able to download, status_code is :", r.status_code)
+            if retry > 0:
+                print('retry')
+                download(url, retry - 1)
+            else:
+                return None
+    except Exception as e:
+        print('exception happend!')
+        print(e)
+        if retry > 0:
+            download(url, retry - 1)
+
+        else:
+            return None
+
+
+def download_mp3(url):
+    headers_temp = {
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3",
+        "Accept-Encoding": "gzip, deflate",
+        "Accept-Language": "zh,en;q=0.9,en-US;q=0.8,zh-CN;q=0.7",
+        "Cache-Control": "no-cache",
+        # "Connection": "keep-alive",
+        # "Cookie": "__cfduid=d996f76d6c8f4b2ddaf74e4c8d0980f0e1558946190; Hm_lvt_74e11efe27096f6ef1745cd53f168168=1558946189,1559005340; blk=0; Hm_lpvt_74e11efe27096f6ef1745cd53f168168=1559005649",
+        "Host": "f2.htqyy.com",
+        "Pragma": "no-cache",
+        "Upgrade-Insecure-Requests": "1",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36",
+    }
+    try:
+        r = requests.get(url=url,headers=headers_temp)
+    except Exception as e:
+        return None
+
+    else:
+        return r
+
 
 def getUrl(url):
-	r=download(url)
-	if not r:
-		return None
+    r = download(url)
+    if not r:
+        return None
 
-	content = r.text
+    content = r.text
 
-	# url='http://f1.htqyy.com/play6/187/m4a/2'
-	# download(url)	
+    try:
+        fileHost = re.findall('var fileHost="(.*?)";', content)[0]
+    except Exception as e:
+        return None
 
-	# with open('source.html','r') as f:
-	# 	content=f.read()
-	# 	tree= etree.HTML(content,parser=etree.HTMLParser(encoding='utf-8'))
-	# print tree.xpath('//title/text()')[0]
-	fileHost=re.findall('var fileHost="(.*?)";',content)[0]
-	mp3=re.findall('var mp3="(.*?)";',content)[0]
-	# print fileHost
-	# print mp3
-	url=fileHost+mp3
-	print url
-	filename=re.findall('var bdText = "(.*?)";',content)[0]
-	filename=re.sub('/','_',filename)
-	post_fix=re.findall('format = "(.*?)"',content)[0]
-	music_file =  filename.replace(' ','_')+'.'+post_fix
-	# print music_file
-	
-	r_music=download(url)
-	if (not os.path.exists(music_file)) or (not r_music):
-		saveToFile(r_music,music_file)
+    try:
+        mp3 = re.findall('var mp3="(.*?)";', content)[0]
+    except Exception as e:
+        return None
 
-def htqyy(start=1,end=7):
-	base_url='http://www.htqyy.com/play/'
-	url_new='http://www.htqyy.com/genre/musicList/9?pageIndex={}&pageSize=20&order=hot'
-	for page in range(start,end):
-		print 'page {} is downloading'.format(page)
-		r=download(url_new.format(page))
-		# print r.text
-		if not r:
-			print "Exit! Been block"
-			continue
-		tree=etree.HTML(r.text,parser=etree.HTMLParser(encoding='utf-8'))
-		music_num=tree.xpath('//li[@class="mItem"]/input/@value')
-		# print len(music_num)
-	# # getUrl(url)
-	# seed_url='http://www.htqyy.com/genre/9'
-	# # r=download(seed_url)
+    url = fileHost + mp3
+    print(url)
+    filename = re.findall('var bdText = "(.*?)";', content)[0]
+    filename = re.sub('/', '_', filename)
+    post_fix = re.findall('format = "(.*?)"', content)[0]
+    music_file = filename.replace(' ', '_') + '.' + post_fix
+    if os.path.exists(music_file):
+        print('文件已经存在')
+        return None
 
-	# tree=etree.HTML(r.text,parser=etree.HTMLParser(encoding='utf-8'))
-	# music_num=tree.xpath('//input[(@type="checkbox") and @name="checked" and (@checked="checked")]/@value')
-	# # print len(music_num)
-		for i in music_num:
-			url=base_url+i
-			print url
-			getUrl(url)
+    r_music = download_mp3(url)
+    if r_music:
+        saveToFile(r_music, music_file)
 
-def main():
-	htqyy()
-	# getUrl()
-	# download()
+
+def htqyy(music_type,start=1, end=11):
+    base_url = 'http://www.htqyy.com/play/'
+
+    # 修改下面的url
+    # music_type = 3
+    url_new = 'http://www.htqyy.com/genre/musicList/{}?pageIndex={}&pageSize=20&order=hot'
+    for page in range(start, end):
+        print('page {} is downloading'.format(page))
+        r = download(url_new.format(music_type, page))
+
+        if not r:
+            print("Exit! Been block")
+            continue
+        tree = etree.HTML(r.text, parser=etree.HTMLParser(encoding='utf-8'))
+        music_num = tree.xpath('//li[@class="mItem"]/input/@value')
+
+        for i in music_num:
+            url = base_url + i
+            print(url)
+            getUrl(url)
+
+
+def main(music_type):
+    htqyy(music_type)
 
 
 if __name__ == '__main__':
-	data_path = os.path.join(os.path.dirname(__file__),'data')
-	if not os.path.exists(data_path):
-		os.mkdir(data_path)
-	os.chdir(data_path)
-	main()
-	print 'End'
+    music_type = sys.argv[1]
+
+    data_path = os.path.join(os.path.dirname(__file__), 'data')
+    if not os.path.exists(data_path):
+        os.mkdir(data_path)
+    os.chdir(data_path)
+    main(music_type)
+    print('End')
