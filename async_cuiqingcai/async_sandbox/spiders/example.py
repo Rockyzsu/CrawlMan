449c91b3c613b2d182ca5e35097b7378e62ae878
Rocky Chen <jinweizsu@gmail.com>
2019-08-21 15:52:08 +0800

    remove

--- async_cuiqingcai/async_sandbox/spiders/example.py
+++ async_cuiqingcai/async_sandbox/spiders/example.py
@@ -7,6 +7,8 @@ from scrapy import Request
 import logging
 from async_sandbox.items import AsyncSandboxItem
 import redis
+from scrapy.xlib.pydispatch import dispatcher
+from scrapy import Request, signals
 
 class ExampleSpider(scrapy.Spider):
     name = 'example'
@@ -83,39 +85,71 @@ class RedisSubcribe(scrapy.Spider):
         super(RedisSubcribe,self).__init__(*args,**kwargs)
 
         # initial redis subscriber
-
-        self.conn = redis.Redis(host='10.18.6.46')
+        # pool = redis.ConnectionPool()
+        self.conn = redis.Redis(host='10.18.6.46',decode_responses=True)
         self.subscribe_channel='cuiqingcai'
         self.pub = self.conn.pubsub()
         self.pub.subscribe(self.subscribe_channel)
         self.pub.parse_response()
         print('initial')
 
+        self.timeout = 30
+        dispatcher.connect(self.spider_idle, signals.spider_idle)
+
+
+    # 订阅者模式 过期 掉线
+    # def start_requests(self):
+    #     # start_page = 0
+
+
+    #     while 1:
+    #         print('waiting for publish')
+    #         msg = self.pub.parse_response()
+    #         content = str(msg[2],'utf-8')
+    #         print(content)
+
+    #         if content=='0':
+    #             break
+
+    #         yield Request(
+    #             url=self.BASE_URL.format(content),
+    #             meta={'page': content},
+    #             dont_filter=True,
+    #         )
+    #     yield None
+
     def start_requests(self):
-        # start_page = 0
-
-
+        
         while 1:
-            print('waiting for publish')
-            msg = self.pub.parse_response()
-            content = str(msg[2],'utf-8')
-            print(content)
+            print('waiting for list')
+            try:
+                msg = self.conn.brpop('page_num',timeout=self.timeout) # 5分钟一次
+            except Exception as e:
+                print(e)
+                self.conn = redis.Redis(host='10.18.6.46',decode_responses=True)
+                msg = self.conn.brpop('page_num',timeout=self.timeout) # 5分钟一次
+                
+            
+            if msg:
+            
+                page = msg[1]
+                yield Request(
+                url=self.BASE_URL.format(page),
+                meta={'page': page},
+                )
 
-            if content=='0':
-                break
+            else:
+                print(f'waiting for receive data {datetime.datetime.now()}')
 
-            yield Request(
-                url=self.BASE_URL.format(content),
-                meta={'page': content},
-                dont_filter=True,
-            )
-        yield None
+                self.conn = redis.Redis(host='10.18.6.46',decode_responses=True)
+
 
     def parse(self, response):
 
         page = response.meta['page']
         # print(response.status_code)
         print(response.text)
+        
         if page ==0:
             print('get exit signal')
             exit()
@@ -137,9 +171,7 @@ class RedisSubcribe(scrapy.Spider):
                 meta={'item': item}
                 )
 
-        
-
-
+    
     def parse_item(self, response):
         item = response.meta['item']
         author = response.xpath(
@@ -155,7 +187,7 @@ class RedisSubcribe(scrapy.Spider):
 
         item['author'] = author
         item['created_at'] = created_at
-        item['content'] = content
+        # item['content'] = content
         visited=re.sub('浏览','',visited)
         item['visited'] = visited
         comment=re.sub('评论','',comment)
@@ -164,3 +196,8 @@ class RedisSubcribe(scrapy.Spider):
         item['crawltime'] = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
         yield item
 
+    # 设置idle的时候
+    def spider_idle(self, spider):
+
+        print('idle status , try go to visit')
+        raise DontCloseSpider("Stayin' alive")
